d46cebfe4d20c89cd35af8895587a30843f7a456
oleg kotov <oleg@farpost.com>
2014-12-14 23:40:00 +1000

    Allow matrix object to have mutable state. Matrix value  now can be changed any time the same way as in the example.

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,24 @@
-## This module defines two functions:
-## makeCacheMatrix: This function creates a special "matrix" object that can cache 
-##                  its inverse.
-## cacheSolve: This function computes the inverse of the special "matrix" returned 
-##             by makeCacheMatrix above. If the inverse has already been calculated 
-##             (and the matrix has not changed), then cacheSolve should retrieve the
-##             inverse from the cache.
+######################################################################################
+##
+## Matrix inversion is usually a costly computation and their may be some benefit
+## to caching the inverse of a matrix rather than compute it repeatedly. The 
+## functions below implements such functionality.
+## 
 ######################################################################################
 
 
-## Returns list represents matrix and  operations on it. Matrix is
-## immutable in terms of creation, it means matrix value can be set only
-## one time while list creation. Cache value can be changed arbitrary 
-## number of times.
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
   im <- NULL
   getMatrix <- function() x
-  getInvertedMatrix <- function () im
-  setInvertedMatrix <- function (invMatrix) {
-    im <<- invMatrix
+  setMatrix <- function(m) {
+    x <<- NULL
+    im <<- NULL
   }
+  
+  getInvertedMatrix <- function () im
+  setInvertedMatrix <- function (invMatrix) im <<- invMatrix
     
   list(getMatrix = getMatrix, 
               setInvertedMatrix = setInvertedMatrix, 
@@ -27,8 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## return cached inverted matrix or in case of cache returns empty value,
-## calculates inverted matrix and caches it value
+## This function computes the inverse of the special "matrix" returned 
+## by makeCacheMatrix above. If the inverse has already been calculated 
+## (and the matrix has not changed), then cacheSolve should retrieve the
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   im <- x$getInvertedMatrix() 
