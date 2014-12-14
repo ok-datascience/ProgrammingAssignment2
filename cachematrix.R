07837e7efe2cacf19ddbb7cd07346ce61bca5d9f
oleg kotov <oleg@farpost.com>
2014-12-14 23:21:16 +1000

    Implemented required functions: makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This module defines two functions:
+## makeCacheMatrix: This function creates a special "matrix" object that can cache 
+##                  its inverse.
+## cacheSolve: This function computes the inverse of the special "matrix" returned 
+##             by makeCacheMatrix above. If the inverse has already been calculated 
+##             (and the matrix has not changed), then cacheSolve should retrieve the
+##             inverse from the cache.
+######################################################################################
 
-## Write a short comment describing this function
+
+## Returns list represents matrix and  operations on it. Matrix is
+## immutable in terms of creation, it means matrix value can be set only
+## one time while list creation. Cache value can be changed arbitrary 
+## number of times.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im <- NULL
+  getMatrix <- function() x
+  getInvertedMatrix <- function () im
+  setInvertedMatrix <- function (invMatrix) {
+    im <<- invMatrix
+  }
+    
+  list(getMatrix = getMatrix, 
+              setInvertedMatrix = setInvertedMatrix, 
+              getInvertedMatrix = getInvertedMatrix)
 }
 
 
-## Write a short comment describing this function
+## return cached inverted matrix or in case of cache returns empty value,
+## calculates inverted matrix and caches it value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  im <- x$getInvertedMatrix() 
+  
+  if (is.null(im)){
+    im <-solve(x$getMatrix())
+    x$setInvertedMatrix(im)
+  }
+  
+  im
 }
